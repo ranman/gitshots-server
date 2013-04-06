3176587e821a990d584c558f27f8b2ec74211fbb
J. Randall Hunt <randallhunt@gmail.com>
2013-04-06 02:47:21 -0400

    I think this might work

--- post-commit.py
+++ post-commit.py
@@ -1,27 +1,43 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env python
+import os
 import subprocess
 import time
 from datetime import datetime
 from pymongo import MongoClient
 from bson import binary
-db = MongoClient().gitscraper
+host = 'localhost'
+port = 27017
+database = 'gitscraper'
+auth = ('gitscraper', 'gitscraper')
 gitshots_path = '~/.gitshots/'
+image_command = 'imagesnap -q -w .5'
+author = 'ranman'
+db = MongoClient(host, port, database)
+db.authenticate(*auth)
+# filename is unix epoch time
 filename = str(time.mktime(datetime.now().timetuple()))[:10] + '.jpg'
-subprocess.check_output('imagesnap', '-q', '-w', '.5',
-                        ''.join(gitshots_path, filename), shell=False)
-with open(filename, 'rb') as f:
-    data = dict()
+subprocess.check_output([image_command.split(' '),
+                        ''.join(gitshots_path, filename)], shell=False)
+imgpath = path = os.path.abspath(os.path.expanduser(gitshots_path + filename))
+data = dict()
+with open(imgpath, 'rb') as f:
     # binary, yo
     data['img'] = binary.Binary(f.read())
-    # timestamp
-    data['ts'] = datetime.fromtimestamp(int(filename[:10]))
-    # commit message
-    data['msg'] = subprocess.check_output(
-        ["git", "log", "-n", "1", "HEAD", "--format=format:%s%n%b"],
-        shell=False)
-    # project name or document
-    data['project'] = ''
-    # diff stats
-    data['dstats']
-    #db.gitshots.insert(data)
 
+data['author'] = 'ranman'
+# timestamp
+data['ts'] = datetime.fromtimestamp(int(filename[:10]))
+# commit message
+data['msg'] = subprocess.check_output(
+    ["git", "log", "-n", "1", "HEAD", "--format=format:%s%n%b"],
+    shell=False)
+# project name or document
+data['project'] = os.basename(os.getcwd())
+# diff stats
+stats = subprocess.check_output(['git', 'diff', 'HEAD~1', '--numstat'])
+stats = stats.split('\n')
+dstats = [dict(zip(['+', '-', 'f'], line.split('\t'))) for line in stats]
+data['dstats'] = dstats
+del data['img']
+print data
+    #db.gitshots.insert(data)
