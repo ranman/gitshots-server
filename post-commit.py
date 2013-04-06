ac72f54c158df37578e3dcaa0fb84339bfefc251
J. Randall Hunt <randallhunt@gmail.com>
2013-04-06 04:14:35 -0400

    good enough for a first pass

--- post-commit.py
+++ post-commit.py
@@ -5,39 +5,42 @@ import time
 from datetime import datetime
 from pymongo import MongoClient
 from bson import binary
-host = 'localhost'
+host = ''
 port = 27017
 database = 'gitscraper'
 auth = ('gitscraper', 'gitscraper')
 gitshots_path = '~/.gitshots/'
-image_command = 'imagesnap -q -w .5'
-author = 'ranman'
-db = MongoClient(host, port, database)
-db.authenticate(*auth)
+# must have space at end
+img_command = 'imagesnap -q '
 # filename is unix epoch time
 filename = str(time.mktime(datetime.now().timetuple()))[:10] + '.jpg'
-subprocess.check_output([image_command.split(' '),
-                        ''.join(gitshots_path, filename)], shell=False)
-imgpath = path = os.path.abspath(os.path.expanduser(gitshots_path + filename))
+imgpath = os.path.abspath(os.path.expanduser(gitshots_path + filename))
+img_command = img_command + imgpath
+# ??? BUG: better way to get this?
+author = 'ranman'
+
+db = MongoClient(host=host, port=port)[database]
+db.authenticate(*auth)
+
+
+subprocess.check_output(img_command.split(' '), shell=False)
 data = dict()
 with open(imgpath, 'rb') as f:
     # binary, yo
     data['img'] = binary.Binary(f.read())
 
-data['author'] = 'ranman'
+data['author'] = author
 # timestamp
 data['ts'] = datetime.fromtimestamp(int(filename[:10]))
-# commit message
+# grab commit message and chop off the last newline
 data['msg'] = subprocess.check_output(
     ["git", "log", "-n", "1", "HEAD", "--format=format:%s%n%b"],
-    shell=False)
+    shell=False).rstrip()
 # project name or document
-data['project'] = os.basename(os.getcwd())
+data['project'] = os.path.basename(os.getcwd())
 # diff stats
 stats = subprocess.check_output(['git', 'diff', 'HEAD~1', '--numstat'])
 stats = stats.split('\n')
-dstats = [dict(zip(['+', '-', 'f'], line.split('\t'))) for line in stats]
+dstats = [dict(zip(['+', '-', 'f'], line.split('\t'))) for line in stats][:-1]
 data['dstats'] = dstats
-del data['img']
-print data
-    #db.gitshots.insert(data)
+db.gitshots.insert(data)
