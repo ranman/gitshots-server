302b3b721fc90c384e0fb63956017c940ad0796c
J. Randall Hunt <randallhunt@gmail.com>
2013-05-08 05:10:56 -0400

    refactor to use post and put for images and commits

--- gitstreamer.py
+++ gitstreamer.py
@@ -1,13 +1,13 @@
-import os
+import cStringIO
+from datetime import datetime
 from collections import defaultdict
 
-from flask import Flask, render_template, send_from_directory, make_response, \
-    request, Response
-
+from flask import Flask, render_template, make_response, request, Response
 from flask.ext.pymongo import PyMongo
 from flask.ext.cache import Cache
-
-from bson.json_util import dumps
+from bson.json_util import dumps, loads
+from bson import binary
+import Image
 
 
 def request_wants_json():
@@ -31,16 +31,12 @@ app.config.update(
     MONGO_USERNAME='gitstreamer',
     MONGO_PASSWORD='gitstreamer',
     CACHE_TYPE='filesystem',
-    CACHE_DIR='static/imgs'
+    CACHE_DIR='static/imgs',
+    UPLOAD_FOLDER='uploads',
+    MAX_CONTENT_LENGTH=4 * 1024 * 1024  # No more than 4MB per file
 )
 cache = Cache(app)
 mongo = PyMongo(app)
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(
-        os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 
 @app.errorhandler(404)
@@ -48,27 +44,67 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.route('/post_image', methods=['POST'])
+def post_image():
+    f = request.files['photo']
+    if f:
+        imgstr = cStringIO.StringIO(f.stream.read())
+        img = Image.open(imgstr)
+        img.convert('RGB')
+        img.thumbnail((300, 300))
+        imgbuf = cStringIO.StringIO()
+        img.save(imgbuf, format='JPEG')
+        gitshot = dict()
+        gitshot['img'] = binary.Binary(imgbuf.getvalue())
+        return str(mongo.db.gitshots.insert(gitshot))
+    return 400
+
+
+@app.route('/post_commit/<ObjectId:gitshot_id>', methods=['PUT'])
+def post_commit(gitshot_id):
+    data = loads(request.data)
+    data['ts'] = datetime.fromtimestamp(data['ts'])
+    gitshot = mongo.db.gitshots.find_one_or_404(gitshot_id)
+    gitshot.update(data)
+    return str(mongo.db.gitshots.save(gitshot))
+
+
 @app.route('/img/<ObjectId:gitshot_id>')
 @cache.memoize(3600)  # cache for 1 hour
 def render_image(gitshot_id):
-    img = mongo.db.gitshots.find_one_or_404(gitshot_id, {'img': True})['img']
-
     def wsgi_app(environ, start_response):
         start_response('200 OK', [('Content-type', 'image/jpeg')])
         return img
 
-    return make_response(wsgi_app)
+    gitshot = mongo.db.gitshots.find_one_or_404(gitshot_id, {'img': True})
+    if 'img' in gitshot:
+        img = gitshot['img']
+        return make_response(wsgi_app)
+    else:
+        img = open('static/no_image.jpg')
+        return make_response(wsgi_app)
 
 
 @app.route('/user/<username>')
 def user_profile(username):
     gitshots = mongo.db.gitshots.find({'author': username}, {'img': False})
     if request_wants_json():
-        return jsonify(items=[gitshot for gitshot in gitshots])
+        return jsonify(items=list(gitshots))
     ret = defaultdict(list)
     for gitshot in gitshots:
         ret[gitshot['project']].append(gitshot)
     return render_template('user.html', gitshots=ret)
+
+
+@app.route('/project/<project>')
+def project(project):
+    gitshots = mongo.db.gitshots.find({'project': project}, {'img': False})
+    if request_wants_json():
+        return jsonify(items=[list(gitshots)])
+    ret = defaultdict(list)
+    for gitshot in gitshots:
+        ret[gitshot['project']].append(gitshot)
+    return render_template('project.html', gitshots=ret)
 
 
 @app.route('/')
--- post-commit.py
+++ post-commit.py
@@ -5,13 +5,11 @@ import os
 import getpass
 import subprocess
 import time
+import json
+import requests
 from datetime import datetime
-from pymongo import MongoClient
-from bson import binary
-host = 'mercury.ranman.org'
-port = 27017
-database = 'gitstreamer'
-auth = ('gitstreamer', 'gitstreamer')
+
+
 gitshots_path = '~/.gitshots/'
 # must have space at end
 img_command = 'imagesnap -q '
@@ -21,19 +19,15 @@ imgpath = os.path.abspath(os.path.expanduser(gitshots_path + filename))
 img_command = img_command + imgpath
 author = getpass.getuser()
 
-db = MongoClient(host=host, port=port)[database]
-db.authenticate(*auth)
-
 print "Taking capture into {0}...".format(imgpath)
 subprocess.check_output(img_command.split(' '), shell=False)
 data = dict()
 with open(imgpath, 'rb') as f:
-    # binary, yo
-    data['img'] = binary.Binary(f.read())
+    img = f.read()
 
 data['author'] = author
 # timestamp
-data['ts'] = datetime.fromtimestamp(int(filename[:10]))
+data['ts'] = int(filename[:10])
 # grab commit message and chop off the last newline
 data['msg'] = subprocess.check_output(
     ["git", "log", "-n", "1", "HEAD", "--format=format:%s%n%b"],
@@ -45,5 +39,13 @@ stats = subprocess.check_output(['git', 'diff', 'HEAD~1', '--numstat'])
 stats = stats.split('\n')
 dstats = [dict(zip(['+', '-', 'f'], line.split('\t'))) for line in stats][:-1]
 data['dstats'] = dstats
-db.gitshots.insert(data)
-print "Data pushed"
+response = requests.post(
+    'http://127.0.0.1:5000/post_image',
+    files={'photo': ('photo', img)}
+)
+print "Image pushed: {0}".format(response.text)
+response = requests.put(
+    'http://127.0.0.1:5000/post_commit/' + response.text,
+    data=json.dumps(data)
+)
+print "Data pushed: {0}".format(response.text)
