f3643cb972d93aa5d2cd1d9f953b3283efefe442
Morgante Pell <morgante.pell@morgante.net>
2014-02-02 12:30:33 +0400

    configurable basic auth

--- app.py
+++ app.py
@@ -26,28 +26,6 @@ def request_wants_json():
     return best == jsonstr and \
         request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
 
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == 'admin' and password == 'secret'
-
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
-
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -57,6 +35,23 @@ mongo = PyMongo(app)
 
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
+def check_auth(username, password):
+    return username == app.config['AUTH_USERNAME'] and password == app.config['AUTH_PASSWORD']
+
+def authenticate():
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if (app.config['AUTH_USERNAME'] and app.config['AUTH_PASSWORD']) and (not auth or not check_auth(auth.username, auth.password)):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 @app.template_filter()
 def commitmsg(value):
@@ -103,6 +98,7 @@ def put_commit(gitshot_id):
 
 
 @app.route('/gitshot/<ObjectId:gitshot_id>.jpg')
+@requires_auth
 @cache.memoize(3600)  # cache for 1 hour
 def render_image(gitshot_id):
     def wsgi_app(environ, start_response):
@@ -119,6 +115,7 @@ def render_image(gitshot_id):
 
 
 @app.route('/user/<username>')
+@requires_auth
 def user_profile(username):
     limit = int(request.args.get('limit', 10))
     sort = request.args.get('sort', 'ts')
@@ -142,6 +139,7 @@ def user_profile(username):
 
 
 @app.route('/project/<project>')
+@requires_auth
 def project(project):
     limit = int(request.args.get('limit', 100))
     sort = request.args.get('sort', 'ts')
@@ -158,6 +156,7 @@ def project(project):
 
 
 @app.route('/gitshot/<ObjectId:gitshot_id>.html')
+@requires_auth
 def gitshot(gitshot_id):
     gitshot = mongo.db.gitshots.find_one(ObjectId(gitshot_id))
     return render_template('commit.html', gitshot=gitshot)
