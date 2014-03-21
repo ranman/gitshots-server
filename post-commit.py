e2deb89c40529f9df5e232736dc608db3038ba7c
J. Randall Hunt <randallhunt@gmail.com>
2014-03-21 03:44:41 -0400

    post-commit major refactor

--- post-commit.py
+++ post-commit.py
@@ -1,92 +1,148 @@
-# !/usr/bin/env python2.7
-# this is an awesome githook created by randall at 4am.
-# it has since been added to by jessepollak
-# TODO: better location stuff
-# TODO: refactor into methods so you can use to upload failed commits
-
+#!/usr/bin/env python2.7
 import os
-import getpass
+import sys
+# if we're rebasing just quit
+if os.path.isdir('.git/rebase-merge'):
+    sys.exit()
+
+# try to fork as soon as possible to not block shell
+# try:
+#     if os.fork():  # will not work on windows
+#         sys.exit()
+# except AttributeError:
+#     pass
+
 import subprocess
 import calendar
 import json
-import sys
 import requests
 from datetime import datetime
 
-if os.path.isdir('.git/rebase-merge'):
-    sys.exit()
-try:
-    if os.fork():  # will not work on windows
-        sys.exit()
-except AttributeError:
-    pass
-
-GITSHOTS_PATH = '~/.gitshots/'
-GITSHOTS_SERVER_URL = os.environ.get(
-    'GITSHOTS_SERVER_URL', 'http://gitshots.ranman.org')
-
-GITSHOTS_IMAGE_CMD = 'imagesnap'
-GITSHOTS_IMAGE_CMD_ARGS = ['-q']
-LOCATION_URI = 'http://where.ranman.org/current_location.json'
+GITSHOTS_PATH = os.getenv('GITSHOTS_PATH', '~/.gitshots/')
+GITSHOTS_SERVER_URL = os.getenv(
+    'GITSHOTS_SERVER_URL',
+    'http://gitshots.ranman.org')
+GITSHOTS_IMAGE_CMD = os.getenv(
+    'GITSHOTS_IMG_CMD',
+    'imagesnap -q ')
+LOCATION_URI = os.getenv(
+    'LOCATION_URI',
+    'http://where.ranman.org/current_location.json')
 
 # ensure directory exists
 if not os.path.exists(os.path.expanduser(GITSHOTS_PATH)):
     os.makedirs(os.path.expanduser(GITSHOTS_PATH))
 
+
+def run_command(command):
+    """Command helper"""
+    return subprocess.check_output(command.split()).rstrip()
+
+#get the top-level directory for this repo:
+failed_path = os.path.join(
+    run_command('git rev-parse --show-toplevel'),
+    '.git/failed_gitshots')
+
+
 # filename is unix epoch time
 filename = str(calendar.timegm(datetime.now().utctimetuple())) + '.jpg'
 imgpath = os.path.abspath(os.path.expanduser(GITSHOTS_PATH + filename))
-GITSHOTS_IMAGE_CMD_ARGS.append(imgpath)
-# this may need to change
-img_command = [GITSHOTS_IMAGE_CMD] + GITSHOTS_IMAGE_CMD_ARGS
+img_command = GITSHOTS_IMAGE_CMD + imgpath
 
-author = getpass.getuser()
-
-print("Taking capture into {0}...".format(imgpath))
-try:
-    subprocess.check_output(img_command, shell=False)
-except OSError as e:
-    if e.errno == os.errno.ENOENT:
-        msg = "Please run `brew install {cmd}` before using gitshots.".format(
-            cmd=GITSHOTS_IMAGE_CMD
-        )
-        raise Exception(msg)
-    else:
-        raise
-
-with open(imgpath, 'rb') as f:
-    img = f.read()
-
-data = {
-    'author': author,
-    # get the timestamp
-    'ts': int(filename[:10]),
-    # grab commit message and chop off the last newline
-    'msg': subprocess.check_output(
-        'git log -n 1 HEAD --format=format:%s%n%b'.split(),
-        shell=False).rstrip(),
-    # get the shaw 1 of this commit
-    'sha1': subprocess.check_output(
-        'git rev-parse HEAD'.split(), shell=False).rstrip(),
-    # project name or document
-    'project': os.path.basename(os.getcwd()),
-}
+author = run_command('git config github.user')
+if not author:
+    print('run git config --global github.user <user>')
+    sys.exit(1)
 
 
-# this command should be empty if this is the first comit
-initial_commit = subprocess.check_output(
-    'git rev-list --min-parents=1 HEAD'.split(), shell=False)
-if not initial_commit:
-    data['dstats'] = 'initial commit'
-else:
-    stats = subprocess.check_output(
-        'git diff --cached --numstat HEAD~'.split(),
-        shell=False)
+def post_gitshots(gitshots):
+    if GITSHOTS_SERVER_URL:
+        for gitshot in gitshots:
+            img = open(gitshot['img'])
+            del gitshot['img']
+            data = json.dumps(gitshot, ensure_ascii=False)
+            try:
+                response = requests.post(
+                    GITSHOTS_SERVER_URL + '/post_image',
+                    files={'photo': ('photo', img)}
+                )
+                response.raise_for_status()
+                response = requests.put(
+                    GITSHOTS_SERVER_URL + '/put_commit/' + response.text,
+                    data=data
+                )
+                response.raise_for_status()
+                cleanup(gitshot)
+            except:
+                print("Unable to upload gitshots, saving for later")
+                save_gitshot(gitshot)
+
+
+def save_gitshot():
+    if not os.path.exists(failed_path):
+        os.makedirs(failed_path)
+
+
+def get_failures():
+    if os.path.exists(failed_path):
+        return os.listdir(failed_path)
+
+
+def cleanup(gitshot):
+    pass
+
+
+def collect_stats():
+    data = {
+        'author': author,
+        # get the timestamp
+        'ts': int(filename[:10]),
+        # grab commit message and chop off the last newline
+        'msg': run_command('git log -n 1 HEAD --format=format:%s%n%b'),
+        # get the shaw 1 of this commit
+        'sha1': run_command('git rev-parse HEAD'),
+        # project name or document
+        'project': os.path.basename(os.getcwd()),
+    }
+    data['dstats'] = file_stats()
+    data['where'] = where()
+    try:
+        data['img'] = take_gitshot()
+    except:
+        print("Unable to take a gitshot! Is your image command configured?")
+    with open(imgpath[:-3] + 'json', 'w') as f:
+        f.write(json.dumps(data, ensure_ascii=False))
+
+    return data
+
+
+def where():
+    # now figure out where we are
+    r = requests.get(LOCATION_URI).json()
+    if r:
+        l = r.get('venue').get('location')
+        where = {
+            'type': 'Point',
+            'coordinates': [l['lng'], l['lat']],
+            'err': '0'
+        }
+        del l['lat']
+        del l['lng']
+        where.update(l)
+        where['ts'] = r['createdAt']
+        return where
+
+
+def file_stats():
+    # this command should be empty if this is the first commit
+    is_not_initial_commit = run_command('git rev-list --min-parents=1 HEAD')
+    if not is_not_initial_commit:
+        return 'initial commit'
+    stats = run_command('git diff --cached --numstat HEAD~')
     stats = stats.split('\n')
-    # split the stats up by number of lines added, removed, and the filename
-    # then chop off the blank line at the end [:-1]
+    # split the stats up by number of lines added/removed
     dstats = []
-    for line in stats[:-1]:
+    for line in stats:
         line = line.split('\t')
         st = {'f': line[2]}
         # we can't get line diffs on binary files.
@@ -97,40 +153,15 @@ else:
             st['+'] = int(line[0])
             st['-'] = int(line[1])
         dstats.append(st)
-    data['dstats'] = dstats
+    return dstats
 
-# now figure out where we are
-r = requests.get(LOCATION_URI).json()
-if r:
-    l = r.get('venue').get('location')
-    data['where'] = {
-        'type': 'Point',
-        'coordinates': [l['lng'], l['lat']],
-        'err': '0'
-    }
-    del l['lat']
-    del l['lng']
-    data['where'].update(l)
-    data['where']['ts'] = r['createdAt']
 
-# chop off the jpg extensions and add json instead
-with open(imgpath[:-3] + 'json', 'w') as f:
-    f.write(json.dumps(data, ensure_ascii=False))
+def take_gitshot():
+    print("Taking capture into {0}...".format(imgpath))
+    run_command(img_command)
+    return imgpath
 
-if GITSHOTS_SERVER_URL and GITSHOTS_SERVER_URL.lower() != "false":
-    data = json.dumps(data, ensure_ascii=False)
-    try:
-        response = requests.post(
-            GITSHOTS_SERVER_URL + '/post_image',
-            files={'photo': ('photo', img)}
-        )
-        response.raise_for_status()
-        response = requests.put(
-            GITSHOTS_SERVER_URL + '/put_commit/' + response.text,
-            data=data
-        )
-        response.raise_for_status()
-    except requests.exceptions.ConnectionError:
-        print('Unable to establish connection, saving data')
-        with open('.git/gitshots_failed', 'a') as f:
-            f.write(imgpath[:-3] + '\n')
+if __name__ == '__main__':
+    gitshots = get_failures() or []
+    gitshots.append(collect_stats())
+    post_gitshots(gitshots)
